037fd69a2b9f0cde848f9c07b2c0608e5ebe974d
DRobertcpp <danila.robert148@gmail.com>
2023-01-07 14:22:55 +0200

    initial commit

---
+++ ComplexExplorer/AboutForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace ComplexExplorer
+{
+    public  class AboutForm : Form
+    {
+        private string[] texte;
+        private RichTextBox txtInfo;
+        private Button btnOK;
+        public AboutForm()
+        {
+            texte = new string[]{ 
+            "Aplica\u021Bie \u00EEn Microsoft Visual C# pentru grafic\u0103 2D"        
+            };
+
+
+            this.Text = "About";
+            this.StartPosition = FormStartPosition.WindowsDefaultLocation;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.Height = 10 * this.Width / 16;
+            this.ControlBox = false;
+
+
+            txtInfo = new RichTextBox();
+            txtInfo.Left = 15;
+            txtInfo.Top = 18;
+            txtInfo.Width = this.Width - 40;
+            txtInfo.Height = 6 + this.Height / 2;
+            txtInfo.Lines = texte;
+            txtInfo.BackColor = Color.Navy;
+            txtInfo.ForeColor = Color.WhiteSmoke;
+            txtInfo.ReadOnly = true;
+            txtInfo.Visible = true;
+            this.Controls.Add(txtInfo);
+
+            btnOK = new Button();
+            btnOK.Text = "OK";
+            btnOK.Left = this.ClientRectangle.Width - btnOK.Width - 20;
+            btnOK.Top = this.ClientRectangle.Height - btnOK.Height - 8;
+
+            btnOK.Visible = true;
+
+            btnOK.Click += new EventHandler(btnOK_Click);
+            this.Controls.Add(btnOK);
+            btnOK.Select();
+        }
+
+        private void btnOK_Click(object sender, EventArgs eArgs)
+        {
+
+            this.Close();
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // AboutForm
+            // 
+            this.ClientSize = new System.Drawing.Size(263, 204);
+            this.Name = "AboutForm";
+            this.Load += new System.EventHandler(this.AboutForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void AboutForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ ComplexExplorer/MyToolbox.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ComplexExplorer
+{
+    public class FormulaLuiCauchy : ComplexForm
+    {
+        static Complex i = new Complex(0, 1);
+        static Complex unuPe2iPi = 1 / (2 *i* Math.PI);
+        static double ro = 2;
+        static double ro2 = ro * ro;
+        static double lat = ro + 0.1;
+
+        Complex gamma(double t)
+        {
+            return Complex.setRoTheta(ro, t);
+        }
+
+        Complex f(Complex z)
+        {
+            return z * z * z + z + 1;
+            //return 1/(z * z * z + z + 1);
+        }
+        void arataMod(int ii, int jj, Complex z)
+        {
+            int k = (int)(100 * z.Ro);
+            setPixel(ii, jj, getColor(k));
+        }
+        void arataArg(int ii, int jj, Complex z)
+        {
+            int k = (int)(512 * (1 + z.Theta / Math.PI));
+            setPixel(ii, jj, getColor(k));
+        }
+        Complex formCauchy(Complex zstar)
+        {
+
+            double a = 0, b = 2 * Math.PI;
+
+            //calculam suma Riemann-Stieltjes
+            //pentru g(z)=f(z)/(z-zstar)
+
+            Complex suma = 0;
+            Complex z0 = gamma(a);
+            for (double t = a; t <= b; t += 0.01)
+            {
+                Complex z1 = gamma(t);
+                suma += f(z1) * (z1 - z0) / (z1 - zstar);
+                z0 = z1;
+            }
+            return unuPe2iPi * suma;
+        }
+        public override void makeImage()
+        {
+
+            setXminXmaxYminYmax(-lat, lat, -lat, lat);
+            setAxis();
+            int imax2 = imax / 2;
+            int jmax2 = jmax / 2;
+            for (int ii = imin; ii <= imax2; ii++)
+            {
+                for (int jj = jmin; jj <= jmax2; jj++)
+                {
+                    Complex z = getZ(2 * ii, 2 * jj);
+                    if (z.Ro2 >= ro2) continue;
+
+                    Complex fExact = f(z);
+                    Complex fAprox = formCauchy(z);
+
+                    arataArg(ii, jj, fExact);
+                    arataMod(ii, jj + jmax2, fExact);
+
+                    arataArg(ii + imax2, jj, fAprox);
+                    arataMod(ii + imax2, jj + jmax2, fAprox);
+                }
+                if (!resetScreen()) return;
+            }
+            setAxis();
+            resetScreen();
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormulaLuiCauchy));
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox
+            // 
+            this.picBox.Image = ((System.Drawing.Image)(resources.GetObject("picBox.Image")));
+            // 
+            // FormulaLuiCauchy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(285, 639);
+            this.Name = "FormulaLuiCauchy";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+
+
+
+
+
+
+
+}
